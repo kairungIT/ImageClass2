6ed99869acc489322ae330193ba60cd640b17992
kairung <kairung@webmail.npru.ac.th>
2023-10-12 21:55:01 +0700

    .commit

--- imgclass2.py
+++ imgclass2.py
@@ -1,11 +1,10 @@
 # Packages required for Image Classification
-import tensorflow as tf
 
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import decode_predictions
-from tensorflow.keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
 
 # To predict the image
 def predict(image1): 
