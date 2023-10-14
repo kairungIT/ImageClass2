29c7b8378a1489fdd8856a0ac1315e88c63a1efd
kairung <kairung@webmail.npru.ac.th>
2023-10-14 14:40:14 +0700

    .commit

--- ml_frontend.py
+++ ml_frontend.py
@@ -11,7 +11,7 @@ from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 import numpy as np
-import cv2
+#import cv2
 from PIL import Image
 from io import BytesIO
 import pandas as pd
